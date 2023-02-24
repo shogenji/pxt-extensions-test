283c3be9e0e2c1d2253ac1a95a60dae133c94767
shogenji <ruirui@ruirui.jp>
2023-02-24 14:14:38 +0900

    ハートを指定した時間だけ表示するブロックの新規追加

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
-})
+/**
+ * Test for Building Extensions.
+ */
+//% color=190 weight=100 icon="\uf0c3" block="Test Blocks"
+namespace test {
+    //% blockId=show_heart
+    //% block="show heart | for %duration x 0.1 seconds"
+    //% duration.min=0 duration.max=1000 duration.defl=1
+    export function showHeart(duration: number): void {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(duration * 100)
+        basic.clearScreen()
+        basic.pause(duration * 100)
+    }
+}
