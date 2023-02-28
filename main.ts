cc19a141620b6efcbb55df4f23f23c36689e5350
shogenji <ruirui@ruirui.jp>
2023-02-28 16:37:32 +0900

    表示時間をミリ秒単位に変更

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@
 //% color=190 weight=100 icon="\uf0c3" block="Test Blocks"
 namespace test {
     //% blockId=show_heart
-    //% block="show heart | for %duration x 0.1 seconds"
-    //% duration.min=0 duration.max=1000 duration.defl=1
+    //% block="show heart | for %duration milliseconds"
+    //% duration.min=0 duration.max=100000 duration.defl=100
     export function showHeart(duration: number): void {
         basic.showIcon(IconNames.Heart)
-        basic.pause(duration * 100)
+        basic.pause(duration)
         basic.clearScreen()
-        basic.pause(duration * 100)
+        basic.pause(duration)
     }
 }
